71b35576d9f7fdbce42af8410ab34772e4d472cc
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-01-13 18:06:55 +0400

    fix

--- back/routes/users/users.service.js
+++ back/routes/users/users.service.js
@@ -1,4 +1,4 @@
-const userModel = require('../../model/expense.model');
+const userModel = require('../../model/user.model');
 
 const getAllUsers = async (req, res) => {
   const users = await userModel
